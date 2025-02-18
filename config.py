441f42c186c59cc19253b5184d7a57c4ec1f9b8c
rotschild <y05597177@gmail.com>
2025-02-18 13:41:01 +0200

    "added main module"

---
+++ config.py
@@ -0,0 +1,30 @@
+# Building characteristics
+NUMBER_THE_FLOORS = 10
+NUMBER_THE_ELEVATORS = 3
+
+# Window size
+WIDTH_WINDOW = 600
+HEIGHT_WINDOW = 800
+
+# Floor size
+WIDTH_FLOOR = 120
+HEIGHT_FLOOR = 80
+ROOF_FLOOR = 7
+
+# elevator size
+WIDTH_ELEVATOR = 60
+HEIGHT_ELEVATOR = 80
+
+# RGB for program colors
+BLACK = (0, 0, 0)
+WHITE = (256, 256, 256)
+GRAY = (128, 128, 128)
+RED = (256, 0, 0)
+GREEN = (0, 256, 0)
+BLUE = (0, 0, 256)
+
+#Fixed times in the program
+FLOOR_CROSSING_TIME = 500
+FLOOR_WAITING_TIME = 2000
+REFRESH_TIME = 60 # Every few milliseconds the main loop ran
+
